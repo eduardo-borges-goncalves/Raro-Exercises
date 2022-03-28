926b7f16b1f6b3393d0cc8712eaa690a5dccb654
Eduardo Borges <alemaobrk1@gmail.com>
2022-03-28 01:55:55 -0300

    correct wrong use api useEffect

--- Semana-06/ex-01/jogo-da-velha/src/App.tsx
+++ Semana-06/ex-01/jogo-da-velha/src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Result } from './components/Result/Result';
 import { Game } from './components/Game';
 import { IObjGame } from './models/IObjGame';
 import { Container } from './components/Container';
 
-let xPlay = true  
+let xPlay = true 
+let xWin= true 
 let winner = "" 
 let result = ""
 
@@ -12,22 +13,22 @@ function App() {
   let objGame: IObjGame = { g1: "", g2: "", g3: "", g4: "", g5: "", g6: "", g7: "", g8: "", g9: "" }
 
   const [gameState, setGameState] = useState <IObjGame> (objGame)
-  const [classNameResult, setClassNameResult] = useState ("areaHidden")
+  let [classNameResult, setClassNameResult] = useState ("areaHidden")
   
   let won = 
-    gameState.g1 === gameState.g2 && gameState.g1 === gameState.g3 && ((gameState.g1 && gameState.g2 && gameState.g3) !== "") ||
-    gameState.g4 === gameState.g5 && gameState.g4 === gameState.g6 && ((gameState.g4 && gameState.g5 && gameState.g6) !== "") ||
-    gameState.g7 === gameState.g8 && gameState.g7 === gameState.g9 && ((gameState.g7 && gameState.g8 && gameState.g9) !== "") ||
-    gameState.g1 === gameState.g4 && gameState.g1 === gameState.g7 && ((gameState.g1 && gameState.g4 && gameState.g7) !== "") ||
-    gameState.g2 === gameState.g5 && gameState.g2 === gameState.g8 && ((gameState.g2 && gameState.g5 && gameState.g8) !== "") ||
-    gameState.g3 === gameState.g6 && gameState.g3 === gameState.g9 && ((gameState.g3 && gameState.g6 && gameState.g9) !== "") ||
-    gameState.g1 === gameState.g5 && gameState.g1 === gameState.g9 && ((gameState.g1 && gameState.g5 && gameState.g9) !== "") ||
-    gameState.g3 === gameState.g5 && gameState.g3 === gameState.g7 && ((gameState.g3 && gameState.g5 && gameState.g7) !== "")
+    gameState.g1 === gameState.g2 && gameState.g1 === gameState.g3 && (gameState.g2 !== "") ||
+    gameState.g4 === gameState.g5 && gameState.g4 === gameState.g6 && (gameState.g5 !== "") ||
+    gameState.g7 === gameState.g8 && gameState.g7 === gameState.g9 && (gameState.g8 !== "") ||
+    gameState.g1 === gameState.g4 && gameState.g1 === gameState.g7 && (gameState.g4 !== "") ||
+    gameState.g2 === gameState.g5 && gameState.g2 === gameState.g8 && (gameState.g5 !== "") ||
+    gameState.g3 === gameState.g6 && gameState.g3 === gameState.g9 && (gameState.g6 !== "") ||
+    gameState.g1 === gameState.g5 && gameState.g1 === gameState.g9 && (gameState.g5 !== "") ||
+    gameState.g3 === gameState.g5 && gameState.g3 === gameState.g7 && (gameState.g5 !== "")
 
   let tie = gameState.g1 !=="" && gameState.g2 !=="" && gameState.g3 !=="" &&
-                     gameState.g4 !=="" && gameState.g5 !=="" && gameState.g6 !=="" &&
-                     gameState.g7 !=="" && gameState.g8 !=="" && gameState.g9 !=="" && 
-                     won === false
+            gameState.g4 !=="" && gameState.g5 !=="" && gameState.g6 !=="" &&
+            gameState.g7 !=="" && gameState.g8 !=="" && gameState.g9 !=="" && 
+            won === false
 
   function xOrBow (event: React.MouseEvent<HTMLButtonElement>) {
     let key:string = event.currentTarget.id
@@ -35,9 +36,11 @@ function App() {
     if (xPlay  && gameState[key] === "" && !won) {
       setGameState({ ...gameState, [key]: "X" })
       xPlay = false
+      xWin = true
     } else if ( gameState[key] === "" && !won ) {
       setGameState({ ...gameState, [key]: "O" })
       xPlay = true
+      xWin = false
     }
   }
 
@@ -46,18 +49,19 @@ function App() {
     setClassNameResult('areaHidden')
   }
 
-  useEffect(() => {
-    xPlay ? winner = "Bola": winner= "X"
-    won && setClassNameResult('newGame')
-    xPlay = true
-  },[won])
-
-  useEffect(() => {
-    tie ? result = "Ihh, empatou! Tente outra vez!": result = "Venceu!";
+  if (won) {
+    result = "Venceu"
+    xWin ? winner = "X": winner= "Bola"
+    classNameResult = 'newGame'
+    xPlay = true 
+  }
+  
+  if (tie) {
+    result = "Ihh, empatou! Tente outra vez!"
     winner = ""
-    tie && setClassNameResult('newGame')
-    xPlay = true
-  }, [tie])
+    classNameResult = 'newGame'
+    xPlay = true 
+  }
 
   return (
     <Container>
